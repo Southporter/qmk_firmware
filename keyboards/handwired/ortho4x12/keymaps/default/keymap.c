110a850bff7a4c55985a30fc618a3fc8635deda2
Shem Sedrick <ssedrick1@gmail.com>
2020-10-30 07:27:09 -0500

    Added colemak, fixed docs and layer naming

--- keyboards/handwired/ortho4x12/keymaps/default/keymap.c
+++ keyboards/handwired/ortho4x12/keymaps/default/keymap.c
@@ -17,6 +17,7 @@
 
 enum ortho4x12_layers {
   _QWERTY,
+  _COLEMAK,
   _DVORAK,
   _LOWER,
   _RAISE,
@@ -25,6 +26,7 @@ enum ortho4x12_layers {
 
 enum ortho4x12_keycodes {
   QWERTY = SAFE_RANGE,
+  COLEMAK,
   DVORAK
 };
 
@@ -50,8 +52,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,       KC_W,   KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTRL, KC_A,       KC_S,   KC_D,    KC_F,  KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,  KC_Z,       KC_X,   KC_C,    KC_V,  KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  ADJUST,  _______, KC_LALT, KC_TAB, KC_LGUI, LOWER, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  ADJUST,  _______, KC_LALT, KC_TAB, KC_LGUI, RAISE, KC_SPC,  LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
+
+/* Colemak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT(
+    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+   KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+   ADJUST, _______, KC_LALT, KC_TAB, KC_LGUI,   RAISE,  KC_SPC,  LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
 
 /* Dvorak
  * ,-----------------------------------------------------------------------------------.
@@ -71,37 +92,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ADJUST, _______, KC_LALT, KC_TAB,  KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |     |      |      | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-[_LOWER] = LAYOUT(
-  KC_GRV,    KC_1,    KC_2,       KC_3,       KC_4,    KC_5,    KC_6,  KC_7, KC_8,    KC_9, KC_0,    KC_BSPC,
-  _______, S(KC_9), S(KC_0), S(KC_LBRC), S(KC_RBRC), KC_EQL,  KC_PIPE,  KC_4, KC_5,    KC_6, KC_QUOT, _______,
-  _______, _______, _______,    KC_LBRC,    KC_RBRC, _______, _______,  KC_1, KC_2,    KC_3, KC_HOME, KC_END ,
-  _______, _______, _______,    _______,    _______, _______, _______,  KC_0, KC_0,  KC_DOT, _______, _______
-),
-
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   ~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |   (  |   )  |   {  |   }  |   =  |   |  |   4  |   5  |   6  |   '  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      |      |      |   [  |   ]  |      |      |   1  |   2  |   3  | HOME |  END |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |      |   0  |   ,  |   .  |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_BSLS, KC_BSPC,
+  KC_TILD,    KC_1,    KC_2,       KC_3,       KC_4,    KC_5,    KC_6,  KC_7,    KC_8,   KC_9, KC_0,    KC_BSPC,
+  _______, S(KC_9), S(KC_0), S(KC_LBRC), S(KC_RBRC), KC_EQL,  KC_PIPE,  KC_4,    KC_5,   KC_6, KC_QUOT, _______,
+  _______, _______, _______,    KC_LBRC,    KC_RBRC, _______, _______,  KC_1,    KC_2,   KC_3, KC_HOME, KC_END ,
+  _______, _______, _______,    _______,    _______, _______, _______,  KC_0, KC_COMM, KC_DOT, _______, _______
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Prev |      |Pg Up |Pg Dn |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = LAYOUT(
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_BSLS, KC_BSPC,
   KC_DEL,  _______, _______, _______,  KC_DQT, _______, _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, _______, KC_PGUP, KC_PGDN,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -120,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT(
   _______, RESET,   DEBUG,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL ,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  DVORAK,  _______,  _______,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  _______,  _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
@@ -138,6 +159,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         print("mode just switched to qwerty and this is a huge string\n");
         set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case COLEMAK:
+      if (record->event.pressed) {
+          set_single_persistent_default_layer(_COLEMAK);
       }
       return false;
       break;
